0ef99bc6266e3b1cc4698acd787f4fcaf87089f6
pushow <qkrehdals55@gmail.com>
2020-05-09 01:16:40 +0900

    chaged1

---
+++ week10/src/week10/week10.java
@@ -0,0 +1,11 @@
+package week10;
+
+public class week10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, Git");
+		System.out.println("changed1");
+	}
+
+}
